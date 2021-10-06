77f0317e461290dcc23e097123b67156c8dfe738
Devendra <phalakdevendra8800@gmail.com>
2021-10-06 14:57:59 +0530

    first copy of my file

---
+++ sample.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void addtwo()
+{
+	int n1,n2;
+	printf("enter the two number");
+	scanf("%d %d",&n1,&n2);
+	printf("sum of the two number is %d",n1+n2);
+}
+
+
+void main()
+{
+
+
+}
